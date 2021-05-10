ad0ff32985d1f6a34cbf29eecec4759c237aebd5
Arcaytus <manu.martinezgalan@gmail.com>
2021-05-10 12:12:53 -0300

    added public folder and modified handlers

--- src/handlers/users/add/index.js
+++ src/handlers/users/add/index.js
@@ -1,17 +1,37 @@
 const database = require('../../../database');
+const { body, validationResult } = require("express-validator");
 
 // POST /api/users
 module.exports = (route) => {
-  route.post('/', (req, res) => {
+  route.post('/', 
+  body("name")
+  .trim()
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .isLength({ min: 3 })
+  .withMessage("Debe tener 3 caracteres"),
+  body("age")
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .isInt()
+  .withMessage("Debe ser un número entero"), (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+      return;
+    }
+
+    const name = req.body.name;
+    const age = req.body.age;
+
     const user = {
-      name: req.body.name,
-      age: req.body.age,
+      name: name.trim(),
+      age: parseInt(age),
     };
 
-    const userName = user.name.toLowerCase().trim();
-
     if (
-      database.DB.some((item) => item.name.toLowerCase().trim() === userName)
+      database.DB.some((item) => item.name.toLowerCase().trim() === user)
     ) {
       res.status(409).json({
         message: '`name` ya existe',
--- src/handlers/users/list.js
+++ src/handlers/users/list.js
@@ -3,6 +3,13 @@ const database = require('../../database');
 // GET /api/users
 module.exports = (route) => {
   route.get('/', (req, res) => {
+    let filterName = req.query.filterName;
+
+    let users = database.DB;
+    if (filterName) {
+      users = users.filter((elem) => elem.name.includes(filterName));
+    }
+
     res.json(database.DB);
   });
 };
